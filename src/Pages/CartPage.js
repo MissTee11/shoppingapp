7789f3178f9743a84c5a52b1b7ffe4c307989e0e
MissTee11 <zizhtarie@gmail.com>
2025-08-05 11:52:58 +0300

    Cart page product container for each product

--- src/Pages/CartPage.js
+++ src/Pages/CartPage.js
@@ -28,48 +28,59 @@ const Cart= () => {
        
     <div>
          <Navbar/>
-         {cart.products.length> 0 ?(
-         <div>
-            <h3 id="motto">SHOPPING CART</h3>
+         {cart.products.length > 0 ? (
+  <div>
+    <h3 id="motto">SHOPPING CART</h3>
 
-            <div className='ProductContainer'>
-                <div className='Left-area'>
-                    {cart.products.map((product)=>(
-                       <div key={product.id} className="CartItem">
-                        <img src={product.thumbnail} alt={product.title} className='productImg'/>
-                            <div className='CartItemDetails'>
-                                <h4>{product.title}</h4>
-                                <p><strong>Description:</strong>{product.description}</p>
-                                <p><strong>Shipping:</strong>{product.shippingInformation}</p>
-                                <p><strong>Warranty:</strong>{product.warrantyInformation}</p>
-                            </div>
-                       </div>
-                    ))}
-                </div>
-                <div className='Right-area'>
-                    {cart.products.map((product)=>(
-                       <div key={product.id}>
-                            <button className="RemoveButton" onClick={()=> handleRemoveFromCart(product.id)}><RiDeleteBinLine /></button>
-                            <div className='CartItemDetails'>
-                                <p>${(product.quantity * product.price).toFixed(2)}</p>
-                                <div className='QuantityCounter'>
-                                    <button className="ButtonA" onClick={()=> handleAddQuantity(product.id)}><FaPlus/></button>
-                                    <p className="Quantity">{product.quantity}</p>
-                                    <button className="ButtonA" onClick={()=> handReduceQuantity(product.id)}><FaMinus/></button>
-                                </div>
-                            </div>
-                       </div>
-                    ))}
-                </div>
+    {cart.products.map((product) => (
+      <div key={product.id} className="ProductContainer">
+        <div className="Left-area">
+          <div className="CartItem">
+            <img src={product.thumbnail} alt={product.title} className="productImg" />
+            <div className="CartItemDetails">
+              <h4>{product.title}</h4>
+              <p><strong>Description:</strong> {product.description}</p>
+              <p><strong>Shipping:</strong> {product.shippingInformation}</p>
+              <p><strong>Warranty:</strong> {product.warrantyInformation}</p>
+            </div>
+          </div>
+        </div>
 
+        <div className="Right-area">
+          <button
+            className="RemoveButton"
+            onClick={() => handleRemoveFromCart(product.id)}
+          >
+            <RiDeleteBinLine />
+          </button>
+          <div className="CartItemDetails">
+            <p>${(product.quantity * product.price).toFixed(2)}</p>
+            <div className="QuantityCounter">
+              <button
+                className="ButtonA"
+                onClick={() => handReduceQuantity(product.id)}
+              >
+                <FaMinus />
+              </button>
+              <p className="Quantity">{product.quantity}</p>
+              <button
+                className="ButtonA"
+                onClick={() => handleAddQuantity(product.id)}
+              >
+                <FaPlus />
+              </button>
             </div>
-           
-            </div>
-            
-        ) : (<div className="CartEmpty">
-         
-            <p id="EmptyCart">CART IS EMPTY!</p>
-            </div>)}
+          </div>
+        </div>
+      </div>
+    ))}
+  </div>
+) : (
+  <div className="CartEmpty">
+    <p id="EmptyCart">CART IS EMPTY!</p>
+  </div>
+)}
+
 
 
             <div className='SummaryWrapper'>
